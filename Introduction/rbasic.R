82573bbf229d892e6f9962a37bc5492e7326f47a
Brayan Valle <brayanvallejaramillo@gmail.com>
2019-04-27 22:01:07 -0500

    basics

---
+++ Introduction/rbasic.R
@@ -0,0 +1,13 @@
+
+#Introduction to R
+
+#Get working directory
+
+getwd()
+
+setwd("E:/Courses/Machine Learning ING1216/")
+
+print("First R class")
+
+#Concat multiple string
+paste("Hello" , "World" , "!")
